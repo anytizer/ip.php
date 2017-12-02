5f494ce6b609a3d4ab16b7a011d07753771edc96
bimal <gist@bimal.org.np>
2017-12-02 12:59:27 -0700

    php7 file

--- readme.md
+++ readme.md
@@ -10,6 +10,8 @@ Find out your IP Address
  * Upload public_html/ there.
  * Visit http://ip.example.com/
 
+__Notes__: If you want php7, upload index7.php as index.php.
+
 ## Command Line
 
 	curl ip.example.com
---
+++ src/public_html/index7.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * PHP 7 only
+ */
+$ip_addresses = array();
+
+$ip_addresses[] = $_SERVER["REMOTE_ADDR"]??"";
+$ip_addresses[] = $_SERVER["HTTP_CLIENT_IP"]??"";
+$ip_addresses[] = $_SERVER["HTTP_X_FORWARDED_FOR"]??"";
+$ip_addresses[] = $_SERVER["HTTP_X_CLUSTER_CLIENT_IP"]??"";
+$ip_addresses[] = $_SERVER["HTTP_FORWARDED_FOR"]??"";
+
+$ip_addresses = array_filter($ip_addresses);
+$ip_addresses = array_unique($ip_addresses);
+
+echo implode(";", $ip_addresses);
