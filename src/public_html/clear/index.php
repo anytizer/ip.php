30c6c67f609632a4c588810eb3a10e13fc358d97
bimal <gist@bimal.org.np>
2018-01-19 00:35:29 -0700

    cleanup

--- src/public_html/clear/index.php
+++ src/public_html/clear/index.php
@@ -1,8 +1,6 @@
 <?php
 require_once("../inc.config.php");
 
-$_SERVER["HTTP_AGENT"] = !empty($_SERVER["HTTP_AGENT"])?$_SERVER["HTTP_AGENT"]:"";
-
 $statement = $pdo->prepare("TRUNCATE TABLE `ip_logs`;");
 $params = array(
 );
